1e99111bfb56b21c3443d5ba120f3b20b7dec8fd
Devin Mounts and Eric Shull <eric@>
2018-08-15 10:53:21 -0700

    add keg list component and display name of keg in html

--- src/app/keg-list/keg-list.component.ts
+++ src/app/keg-list/keg-list.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Keg } from '../models/keg-model'
 
 @Component({
   selector: 'app-keg-list',
   templateUrl: './keg-list.component.html',
   styleUrls: ['./keg-list.component.css']
 })
-export class KegListComponent implements OnInit {
 
-  constructor() { }
+export class KegListComponent  {
+  @Input() childKegList: Keg[];
 
-  ngOnInit() {
+  kegToShow = null;
+
+  showKeg(clickedKeg){
+  if(this.kegToShow == clickedKeg){
+    this.kegToShow = null;
+  } else {
+    this.kegToShow = clickedKeg;
   }
+}
+
+typeColor(currentKeg) {
+  if (currentKeg.type == 'ipa') {
+    return "bg-success";
+  } else if (currentKeg.type == 'stout'){
+    return "bg-warning";
+  } else {
+    return "bg-info";
+  }
+}
+
+
+
 
 }
